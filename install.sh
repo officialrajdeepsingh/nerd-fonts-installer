7d600349c11d154ac81d67067ffbbc83c13a0ebc
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-15 22:16:27 +0530

    update the script

--- install.sh
+++ install.sh
@@ -3,18 +3,19 @@
 
 echo "[-] Download The Nerd $1 fonts [-]"
 echo "#######################"
-if [ -z "$1" ]; then
+if [ -n "$1" ]; then
+    if [ "$(command -v curl)" ]; then
+        echo "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$1.zip"
+        curl -OL "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$1.zip"
+    elif [ "$(command -v wget)" ]; then
+        echo "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$1.zip"
+        wget "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$1.zip"
+    else
+        echo "We cannot find the curl and wget command. First, install the curl and wget command, one of them."
+    fi
+    unzip JetBrainsMono.zip -d ~/.fonts
+    fc-cache -fv
+    echo "done!"
+else
     echo "Please provide the nerd font name. Example: JetBrainsMono"
 fi
-if [ "$(command -v curl)" ]; then
-    echo "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$1.zip"
-    curl -OL "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$1.zip"
-elif [ "$(command -v wget)" ]; then
-    echo "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$1.zip"
-    wget "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$1.zip"
-else
-    echo "We cannot find the curl and wget command. First, install the curl and wget command, one of them."
-fi
-unzip JetBrainsMono.zip -d ~/.fonts
-fc-cache -fv
-echo "done!"
