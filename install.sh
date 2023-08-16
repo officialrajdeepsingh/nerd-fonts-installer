23dcaad365f62fbd3da995413b96b44120b484ff
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-16 18:05:05 +0530

    Adding list of nerd font

--- install.sh
+++ install.sh
@@ -3,22 +3,38 @@
 
 echo "[-] Download The Nerd fonts [-]"
 echo "#######################"
-echo "Enter the Nerd Font Name:"  
-read font_name
-if [ -n "$font_name" ]; then
-    if [ "$(command -v curl)" ]; then
-        echo "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font_name.zip"
-        curl -OL "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font_name.zip"
-    elif [ "$(command -v wget)" ]; then
-        echo "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font_name.zip"
-        wget "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font_name.zip"
+echo "Select Nerd Font"
+fons_list=("Agave" "AnonymousPro" "Arimo" "AurulentSansMono" "BigBlueTerminal" "BitstreamVeraSansMono" "CascadiaCode" "CodeNewRoman" "ComicShannsMono" "Cousine" "DaddyTimeMono" "DejaVuSansMono" "FantasqueSansMono" "FiraCode" "FiraMono" "Gohu" "Go-Mono" "Hack" "Hasklig" "HeavyData" "Hermit" "iA-Writer" "IBMPlexMono" "InconsolataGo" "InconsolataLGC" "Inconsolata" "IosevkaTerm" "JetBrainsMono" "Lekton" "LiberationMono" "Lilex" "Meslo" "Monofur" "Monoid" "Mononoki" "MPlus" "NerdFontsSymbolsOnly" "Noto" "OpenDyslexic" "Overpass" "ProFont" "ProggyClean" "RobotoMono" "ShareTechMono" "SourceCodePro" "SpaceMono" "Terminus" "Tinos" "UbuntuMono" "Ubuntu" "VictorMono")
+PS3="Enter a number: "
+select font_name in "${fons_list[@]}" "Quit";
+ do
+
+    if [ -n "$font_name" ]; then
+        
+        echo "Starting download $font_name nerd font"
+        
+        if [ "$(command -v curl)" ]; then
+            echo "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font_name.zip"
+            curl -OL "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font_name.zip"
+            echo "nzip the $font_name.zip"
+            unzip "$font_name.zip" -d "~/.fonts/$font_name/"
+            fc-cache -fv
+            echo "done!"        
+        elif [ "$(command -v wget)" ]; then
+
+            echo "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font_name.zip"
+            wget "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font_name.zip"
+            echo "nzip the $font_name.zip"
+            unzip "$font_name.zip" -d ~/.fonts
+            fc-cache -fv
+            echo "done!"
+        else
+            echo "We cannot find the curl and wget command. First, install the curl and wget command, one of them."
+            break
+        fi
+
     else
-        echo "We cannot find the curl and wget command. First, install the curl and wget command, one of them."
+        echo "Select the vaild $font_name nerd Font, just type number"
+        continue;
     fi
-    echo "nzip the $font_name.zip"
-    unzip "$font_name.zip" -d ~/.fonts
-    fc-cache -fv
-    echo "done!"
-else
-    echo "Please provide the nerd font name. Example: JetBrainsMono"
-fi
+done
