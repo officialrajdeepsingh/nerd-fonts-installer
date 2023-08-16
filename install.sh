f6b54481df8e2fbac77e9a2be4c7c3faa33cd676
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-16 15:06:28 +0530

    Reading fonts

--- install.sh
+++ install.sh
@@ -1,19 +1,22 @@
 #/bin/bash
 # Select the Nerd Font from https://www.nerdfonts.com/font-downloads
 
-echo "[-] Download The Nerd $1 fonts [-]"
+echo "[-] Download The Nerd fonts [-]"
 echo "#######################"
+echo "Enter the Nerd Font Name:"  
+read font_name
 if [ -n "$1" ]; then
     if [ "$(command -v curl)" ]; then
-        echo "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$1.zip"
-        curl -OL "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$1.zip"
+        echo "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font_name.zip"
+        curl -OL "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font_name.zip"
     elif [ "$(command -v wget)" ]; then
-        echo "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$1.zip"
-        wget "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$1.zip"
+        echo "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font_name.zip"
+        wget "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font_name.zip"
     else
         echo "We cannot find the curl and wget command. First, install the curl and wget command, one of them."
     fi
-    unzip "$1.zip" -d ~/.fonts
+    echo "nzip the $font_name.zip"
+    unzip "$font_name.zip" -d ~/.fonts
     fc-cache -fv
     echo "done!"
 else
