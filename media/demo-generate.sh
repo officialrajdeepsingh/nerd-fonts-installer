1d84939c01592f0677b0618ab365c27c9b07c552
Vladimir nett00n Budylnikov <git@nett00n.org>
2026-06-20 10:50:58 +0400

    demo with vhs!

--- media/demo-generate.sh
+++ media/demo-generate.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
-# docker run --rm -it -v $PWD:/vhs --entrypoint=bash ghcr.io/charmbracelet/vhs
+# docker run --rm -it -e UID=$(id -u) -v $PWD:/vhs --entrypoint=bash ghcr.io/charmbracelet/vhs media/demo-generate.sh
 # ./media/demo-generate.sh
-apt update -qq; apt install curl tar xz-utils -y -qq --no-install-recommends
-mkdir -p "${HOME}/.local/share/"
+apt update -qq; apt install curl tar xz-utils figlet -y -qq --no-install-recommends
+mkdir -p "${HOME}/.local/share/" media/screenshots media/videos
 find "${HOME}/.local/share/fonts" -delete
+mkdir -p media/screenshots/ media/videos/
+vhs media/title.tape
 vhs media/demo.tape
+chown $UID ./media/ -R
