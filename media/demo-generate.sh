b4518b4d8b2737c22662bd6a3dc6a673d5eb6433
Vladimir nett00n Budylnikov <git@nett00n.org>
2026-06-20 13:00:14 +0400

    Better demo!

--- media/demo-generate.sh
+++ media/demo-generate.sh
@@ -2,6 +2,10 @@
 # docker run --rm -it -e UID=$(id -u) -v $PWD:/vhs --entrypoint=bash ghcr.io/charmbracelet/vhs media/demo-generate.sh
 # ./media/demo-generate.sh
 apt update -qq; apt install curl tar xz-utils figlet -y -qq --no-install-recommends
+find ./media/ -name "*.gif" -delete
+find ./media/ -name "*.mp4" -delete
+find ./media/ -name "*.png" -delete
+find ./media/ -name "*.webm" -delete
 mkdir -p "${HOME}/.local/share/" media/screenshots media/videos
 find "${HOME}/.local/share/fonts" -delete
 mkdir -p media/screenshots/ media/videos/
